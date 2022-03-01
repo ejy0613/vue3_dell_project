84bb16c1125bec248cd53cd5a24879c5751f71cc
Joy <799840822@qq.com>
2022-03-01 09:35:42 +0800

    create home page

--- code/README.md
+++ code/README.md
@@ -1,24 +1,37 @@
-# code
+# Vue3 Project Code
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Technology
+
+```markdown
+  normalize.css --- 解决浏览器css表现差异
+```
+
+### 解决图片加载抖动
--- code/src/App.vue
+++ code/src/App.vue
@@ -1,30 +1,83 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="wrapper">
+    <div class="position">
+      <span class="icon-location"></span>
+      深圳市软件产业基地
+    </div>
+    <div class="search">
+      <span class="iconfont icon-search">&#xe699;</span>
+      山姆会员店优惠商品
+    </div>
+    <div class="banner">
+      <img class="banner__img" src="http://www.dell-lee.com/imgs/vue3/banner.jpg" alt="" srcset="">
+    </div>
   </div>
-  <router-view/>
+  <div class="docker">
+    <!-- BEM: block__element--Modifier -->
+    <span class="docker__item docker__item--active">首页</span>
+    <span class="docker__item">购物车</span>
+    <span class="docker__item">订单</span>
+    <span class="docker__item">我的</span>
+  </div>
+  <!-- <router-view/> -->
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+@import './style/viriables.scss';
+@import './style/mixins.scss';
+  .wrapper {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: .5rem;
+    width: 100%;
+    padding: 0 .18rem;
+    box-sizing: border-box;
+    color: $content-fontcolor;
+    .position {
+      padding: .16rem .24rem .1rem 0;
+      line-height: .22rem;
+      font-size: .16rem;
+      @include ellipsis;
+    }
+    .search {
+      line-height: .32rem;
+      background: #f5f5f5;
+      color: #b7b7b7;
+      border-radius: 0.16rem;
+      font-size: .14rem;
+      margin-bottom: .12rem;
+      .iconfont {
+        position: relative;
+        display: inline-block;
+        top: .03rem;
+        padding: 0 0 0 .16rem;
+        font-size: .22rem;
+      }
+    }
+    .banner {
+      &__img {
+        width: 100%;
+      }
     }
   }
-}
+  .docker {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    display: flex;
+    width: 100%;
+    height: 0.49rem;
+    background: #efefef;
+    border-top: .01rem solid #f1f1f1;
+    box-sizing: border-box;
+    &__item {
+      flex: 1;
+      text-align: center;
+      line-height: 0.49rem;
+      &--active {
+        color: #409eff;
+      }
+    }
+  }
 </style>
