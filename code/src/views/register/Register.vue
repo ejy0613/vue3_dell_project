57fe7b790ad823313d103ccf7c322bacfe640902
Joy <799840822@qq.com>
2022-03-14 15:29:00 +0800

    fix: update code

--- code/src/views/register/Register.vue
+++ code/src/views/register/Register.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="wrapper">
-    <img class="wrapper__img" src="http://www.dell-lee.com/imgs/vue3/user.png" alt="">
+    <img class="wrapper__img" src="http://www.dell-lee.com/imgs/vue3/user.png" alt />
     <div class="wrapper__input">
       <input class="wrapper__input__content" placeholder="请输入手机号" v-model="mobile" />
     </div>
     <div class="wrapper__input">
       <input class="wrapper__input__content" placeholder="请输入密码" type="password" v-model="password" />
     </div>
-     <div class="wrapper__input">
-      <input class="wrapper__input__content" placeholder="确认密码" type="password" v-model="passwordRep" />
+    <div class="wrapper__input">
+      <input
+        class="wrapper__input__content"
+        placeholder="确认密码"
+        type="password"
+        v-model="passwordRep"
+      />
     </div>
     <div class="wrapper__login-button" @click="handleRegisterClick">注册</div>
     <div class="wrapper__login-link" @click="handleLoginClick">已有账号去登录</div>
@@ -61,54 +66,55 @@ export default {
 }
 </script>
 <style lang="scss">
-@import '../../style/viriables.scss';
-  .wrapper {
-    position: absolute;
-    top: 50%;
-    left: 0;
-    right: 0;
-    transform: translateY(-50%);
-    &__img {
-      display: block;
-      margin: 0 auto .4rem auto;
-      width: .66rem;
-      height: .66rem;
-    }
-    &__input {
-      height: .48rem;
-      margin: 0 .4rem .16rem .4rem;
-      padding: 0 .16rem;
-      background: #F9F9F9;
-      border: .01rem solid rgba($color: #000000, $alpha: 0.1);
-      border-radius: .06rem;
-      box-sizing: border-box;
-      &__content {
-        line-height: .48rem;
-        width: 100%;
-        border: none;
-        outline: none;
-        background: transparent;
-        font-size: .16rem;
+@import "../../style/viriables.scss";
+.wrapper {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  transform: translateY(-50%);
+  &__img {
+    display: block;
+    margin: 0 auto 0.4rem auto;
+    width: 0.66rem;
+    height: 0.66rem;
+  }
+  &__input {
+    height: 0.48rem;
+    margin: 0 0.4rem 0.16rem 0.4rem;
+    padding: 0 0.16rem;
+    background: #f9f9f9;
+    border: 0.01rem solid rgba($color: #000000, $alpha: 0.1);
+    border-radius: 0.06rem;
+    box-sizing: border-box;
+    &__content {
+      margin-top: 0.12rem;
+      line-height: 0.22rem;
+      width: 100%;
+      border: none;
+      outline: none;
+      background: transparent;
+      font-size: 0.16rem;
+      color: $content-notice-fontcolor;
+      &::placeholder {
         color: $content-notice-fontcolor;
-        &::placeholder {
-          color: $content-notice-fontcolor;
-        }
       }
     }
-    &__login-button {
-      margin: .32rem .4rem .16rem;
-      line-height: .48rem;
-      color: #fff;
-      font-size: .16rem;
-      text-align: center;
-      background: #0091ff;
-      box-shadow: 0 .04rem .08rem rgba(0, 145, 255, 0.32);
-      border-radius: .04rem;
-    }
-    &__login-link {
-      text-align: center;
-      font-size: .14rem;
-      color: $content-notice-fontcolor;
-    }
   }
+  &__login-button {
+    margin: 0.32rem 0.4rem 0.16rem;
+    line-height: 0.48rem;
+    color: #fff;
+    font-size: 0.16rem;
+    text-align: center;
+    background: #0091ff;
+    box-shadow: 0 0.04rem 0.08rem rgba(0, 145, 255, 0.32);
+    border-radius: 0.04rem;
+  }
+  &__login-link {
+    text-align: center;
+    font-size: 0.14rem;
+    color: $content-notice-fontcolor;
+  }
+}
 </style>
